9ed17d6bf641dde7f3db2165ca204f60b23df66e
mauriciogparadox <156235584+mauriciogparadox@users.noreply.github.com>
2024-08-08 13:26:57 -0600

    Update docking.py

--- docking.py
+++ docking.py
@@ -3,6 +3,19 @@ import MDAnalysis as mda
 import numpy as np
 import time
 
+# Function to convert pdb to pdbqt
+
+def convert_pdb_to_pdbqt(input_pdb, output_pdbqt):
+    # Command to convert PDB to PDBQT using OpenBabel
+    command = f'obabel -ipdb {input_pdb} -opdbqt -O {output_pdbqt}'
+
+    # Execute the command using subprocess
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print(f'Conversion successful: {input_pdb} -> {output_pdbqt}')
+    except subprocess.CalledProcessError as e:
+        print(f'Error during conversion: {e}')
+
 # Functions to create box size
 
 def compute_bounding_box(pdb_file):
@@ -35,7 +48,12 @@ def calculate_center_of_mass(pdb_file):
 # Provide files
 
 receptor_file = input("Provide a receptor file name:")
+receptor_pdbqt = f"{receptor_pdb}qt"
 ligand_file = input("Provide a ligand file name:")
+ligand_pdbqt = f"{ligand_file}qt"
+
+convert_pdb_to_pdbqt(receptor_file, receptor_pdbqt)
+convert_pdb_to_pdbqt(ligand_file, ligand_pdbqt)
 
 center, size = compute_bounding_box(receptor_file)
 
@@ -51,11 +69,11 @@ print('Center of receptor:', center_receptor)
 
 v = Vina(sf_name='vina', cpu = -1)
 
-v.set_receptor(receptor_file)
+v.set_receptor(receptor_pdbqt)
 
 v.compute_vina_maps(center=center_receptor, box_size=adjusted_size)
 
-v.set_ligand_from_file(ligand_file)
+v.set_ligand_from_file(ligand_pdbqt)
 
 start_time = time.time()
 
