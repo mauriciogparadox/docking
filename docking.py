2502684d7b6bf81d3473ee373d44435c8eed66d9
mauriciogparadox <mauricio@geneparadox.com>
2024-08-08 14:27:34 -0600

    New changes

--- docking.py
+++ docking.py
@@ -1,7 +1,9 @@
 from vina import Vina
 import MDAnalysis as mda
 import numpy as np
+from Bio.PDB import PDBParser
 import time
+import subprocess
 
 # Function to convert pdb to pdbqt
 
@@ -33,6 +35,8 @@ def compute_bounding_box(pdb_file):
     center = (min_coords + max_coords) / 2
     size = max_coords - min_coords
 
+    return center, size
+
 def adjust_box_size(center, size, padding=5.0):
     # Add padding to the size to ensure the ligand can fully explore the binding site
     adjusted_size = size + padding
@@ -40,7 +44,7 @@ def adjust_box_size(center, size, padding=5.0):
 
 # Function to calculate center of mass
 
-def calculate_center_of_mass(pdb_file):
+def calculate_center_of_mass(structure):
     atoms = [atom for atom in structure.get_atoms()]
     center = [sum(coord) / len(atoms) for coord in zip(*[atom.coord for atom in atoms])]
     return center
@@ -48,20 +52,27 @@ def calculate_center_of_mass(pdb_file):
 # Provide files
 
 receptor_file = input("Provide a receptor file name:")
-receptor_pdbqt = f"{receptor_pdb}qt"
+#receptor_pdbqt = f"{receptor_file}qt"
 ligand_file = input("Provide a ligand file name:")
-ligand_pdbqt = f"{ligand_file}qt"
+#ligand_pdbqt = f"{ligand_file}qt"
 
-convert_pdb_to_pdbqt(receptor_file, receptor_pdbqt)
-convert_pdb_to_pdbqt(ligand_file, ligand_pdbqt)
+#convert_pdb_to_pdbqt(receptor_file, receptor_pdbqt)
+#convert_pdb_to_pdbqt(ligand_file, ligand_pdbqt)
 
 center, size = compute_bounding_box(receptor_file)
 
 center, adjusted_size = adjust_box_size(center, size, padding=5.0)
 
+adjusted_size = [float(dim) for dim in adjusted_size]
+
 print(f"Box Size: {adjusted_size}")
 
-center_receptor = calculate_center_of_mass(receptor_file)
+parser = PDBParser()
+
+receptor_structure = parser.get_structure('receptor', receptor_file)
+
+center_receptor = calculate_center_of_mass(receptor_structure)
+center_receptor = [float(coord) for coord in center_receptor]
 
 print('Center of receptor:', center_receptor)
 
@@ -69,11 +80,11 @@ print('Center of receptor:', center_receptor)
 
 v = Vina(sf_name='vina', cpu = -1)
 
-v.set_receptor(receptor_pdbqt)
+v.set_receptor(receptor_file)
 
 v.compute_vina_maps(center=center_receptor, box_size=adjusted_size)
 
-v.set_ligand_from_file(ligand_pdbqt)
+v.set_ligand_from_file(ligand_file)
 
 start_time = time.time()
 
@@ -85,8 +96,9 @@ elapsed_time = end_time - start_time
 
 print(f"Time taken: {elapsed_time:.2f} seconds")
 
-pose = input("Provide a pose from the list:")
+pose = int(input("Provide a pose from the list:"))
 
 v.write_poses('example_output.pdbqt', n_poses=pose, overwrite=True)
 
 # END
+
